53ac469ec07c62cc22e98cfefe227f7fa221dc67
Alerom2006 <1a1r120061@gmail.com>
2025-07-01 14:43:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,163 +1,120 @@
 define(["jquery"], function ($) {
-  var OrdersCalendar = function () {
-    var self = this;
+  function OrdersCalendar() {
+    // Сохраняем контекст
+    const self = this;
 
-    // Константы и настройки
-    this.FIELD_IDS = {
-      ORDER_DATE: 885453,
-      DELIVERY_RANGE: 892009,
-      EXACT_TIME: 892003,
-      ADDRESS: 887367,
+    // 1. Конфигурация виджета
+    this.config = {
+      FIELD_IDS: {
+        ORDER_DATE: 885453,
+        DELIVERY_RANGE: 892009,
+        EXACT_TIME: 892003,
+        ADDRESS: 887367,
+      },
+      langs: {
+        months: [
+          "Январь",
+          "Февраль",
+          "Март",
+          "Апрель",
+          "Май",
+          "Июнь",
+          "Июль",
+          "Август",
+          "Сентябрь",
+          "Октябрь",
+          "Ноябрь",
+          "Декабрь",
+        ],
+        weekdays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+        widgetName: "Календарь заказов",
+        orderDate: "Дата заказа:",
+        openCalendar: "Открыть календарь",
+        noDeals: "Нет сделок на эту дату",
+        noName: "Без названия",
+        delivery: "Доставка",
+        time: "Время",
+        address: "Адрес",
+      },
     };
 
-    this.langs = {
-      months: [
-        "Январь",
-        "Февраль",
-        "Март",
-        "Апрель",
-        "Май",
-        "Июнь",
-        "Июль",
-        "Август",
-        "Сентябрь",
-        "Октябрь",
-        "Ноябрь",
-        "Декабрь",
-      ],
-      weekdays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
-      widgetName: "Календарь заказов",
-      orderDate: "Дата заказа:",
-      openCalendar: "Открыть календарь",
-      noDeals: "Нет сделок на эту дату",
-      noName: "Без названия",
-      delivery: "Доставка",
-      time: "Время",
-      address: "Адрес",
-    };
-
-    // Состояние виджета
+    // 2. Состояние виджета
     this.state = {
       currentDate: new Date(),
       isLoading: false,
       dealsData: {},
       currentDealId: null,
-      accessToken: null,
+      accessToken: localStorage.getItem("amo_access_token"),
       settings: {
         deal_date_field_id: "885453",
         delivery_range_field: "892009",
       },
+      system: {
+        account: "spacebakery1",
+        entity_id: null,
+      },
     };
+
+    // 3. Основные методы
 
     // Инициализация системы
     this.initSystem = function () {
-      this.system = {
-        account: "spacebakery1",
-        entity_id: null,
-      };
-
       if (
         typeof AmoCRM !== "undefined" &&
         typeof AmoCRM.widgets.system === "function"
       ) {
-        AmoCRM.widgets.system().then(function (systemApi) {
-          self.system = systemApi;
+        return AmoCRM.widgets.system().then(function (systemApi) {
+          self.state.system = systemApi;
           self.state.currentDealId = systemApi.entity_id;
           self.state.accessToken =
-            systemApi.access_token || localStorage.getItem("amo_access_token");
+            systemApi.access_token || self.state.accessToken;
+          localStorage.setItem("amo_access_token", self.state.accessToken);
         });
       }
+      return Promise.resolve();
     };
 
-    // Основные методы
-    this.getDealIdFromUrl = function () {
-      var match = window.location.pathname.match(/leads\/detail\/(\d+)/);
-      return match ? match[1] : null;
-    };
-
-    this.isDealPage = function () {
-      this.state.currentDealId =
-        this.system.entity_id || this.getDealIdFromUrl();
-      return !!this.state.currentDealId;
-    };
-
+    // Загрузка настроек полей
     this.loadFieldIdsFromSettings = function () {
       try {
-        if (this.state.settings.deal_date_field_id) {
-          this.FIELD_IDS.ORDER_DATE =
-            parseInt(this.state.settings.deal_date_field_id) ||
-            this.FIELD_IDS.ORDER_DATE;
+        if (self.state.settings.deal_date_field_id) {
+          self.config.FIELD_IDS.ORDER_DATE =
+            parseInt(self.state.settings.deal_date_field_id) ||
+            self.config.FIELD_IDS.ORDER_DATE;
         }
-        if (this.state.settings.delivery_range_field) {
-          this.FIELD_IDS.DELIVERY_RANGE =
-            parseInt(this.state.settings.delivery_range_field) ||
-            this.FIELD_IDS.DELIVERY_RANGE;
+        if (self.state.settings.delivery_range_field) {
+          self.config.FIELD_IDS.DELIVERY_RANGE =
+            parseInt(self.state.settings.delivery_range_field) ||
+            self.config.FIELD_IDS.DELIVERY_RANGE;
         }
       } catch (e) {
         console.error("Ошибка загрузки ID полей:", e);
       }
     };
 
-    // UI методы
-    this.setupUI = function () {
-      if (this.isDealPage()) {
-        $("#widget_container").addClass("deal-widget-mode");
-        this.loadDealData();
-      } else {
-        $("#currentMonthYear").text(this.getMonthTitle());
-        this.renderCalendar();
-      }
+    // Проверка страницы сделки
+    this.isDealPage = function () {
+      const match = window.location.pathname.match(/leads\/detail\/(\d+)/);
+      self.state.currentDealId =
+        self.state.system.entity_id || (match ? match[1] : null);
+      return !!self.state.currentDealId;
     };
 
-    this.getMonthTitle = function () {
-      return (
-        this.langs.months[this.state.currentDate.getMonth()] +
-        " " +
-        this.state.currentDate.getFullYear()
-      );
-    };
-
-    // Работа с API
-    this.fetchDeals = function (year, month) {
-      if (!this.state.accessToken) return Promise.resolve({});
-
-      var startDate = new Date(year, month, 1).toISOString().split("T")[0];
-      var endDate = new Date(year, month + 1, 0).toISOString().split("T")[0];
-
-      return $.ajax({
-        url: `https://${this.system.account}.amocrm.ru/api/v4/leads`,
-        data: {
-          "filter[custom_fields_values][field_id]": this.FIELD_IDS.ORDER_DATE,
-          "filter[custom_fields_values][from]": startDate,
-          "filter[custom_fields_values][to]": endDate,
-        },
-        headers: {
-          Authorization: `Bearer ${this.state.accessToken}`,
-          "X-Requested-With": "XMLHttpRequest",
-        },
-        timeout: 15000,
-      })
-        .then(this.processDealsData.bind(this))
-        .catch(function (error) {
-          console.error("Ошибка загрузки сделок:", error);
-          return {};
-        });
-    };
-
-    // Callbacks для amoCRM
+    // 4. Callbacks для amoCRM
     this.callbacks = {
       init: function () {
-        try {
-          self.initSystem();
-          self.loadFieldIdsFromSettings();
-          self.setupUI();
-          self.setupEventListeners();
-          return true;
-        } catch (error) {
-          console.error("Ошибка инициализации:", error);
-          self.showError("Ошибка загрузки виджета");
-          return false;
-        }
+        return self
+          .initSystem()
+          .then(function () {
+            self.loadFieldIdsFromSettings();
+            self.setupUI();
+            self.setupEventListeners();
+            return true;
+          })
+          .catch(function (error) {
+            console.error("Ошибка инициализации:", error);
+            return false;
+          });
       },
 
       onSave: function (newSettings) {
@@ -179,7 +136,7 @@ define(["jquery"], function ($) {
         }
       },
 
-      // Остальные обязательные callbacks
+      // Обязательные callback-и
       render: function () {
         return true;
       },
@@ -192,43 +149,59 @@ define(["jquery"], function ($) {
       dpSettings: function () {
         return true;
       },
-      advancedSettings: function () {
-        return true;
-      },
       destroy: function () {
-        return true;
-      },
-      contacts: {
-        selected: function () {
-          return true;
-        },
-      },
-      leads: {
-        selected: function () {
-          return true;
-        },
-      },
-      todo: {
-        selected: function () {
-          return true;
-        },
-      },
-      onAddAsSource: function () {
         return true;
       },
     };
 
-    // Инициализация
-    this.initSystem();
-    this.loadFieldIdsFromSettings();
+    // 5. UI методы
+    this.setupUI = function () {
+      if (self.isDealPage()) {
+        $("#widget_container").addClass("deal-widget-mode");
+        self.loadDealData();
+      } else {
+        $("#currentMonthYear").text(
+          `${
+            self.config.langs.months[self.state.currentDate.getMonth()]
+          } ${self.state.currentDate.getFullYear()}`
+        );
+        self.renderCalendar();
+      }
+    };
 
-    // Экспорт в глобальную область видимости
-    if (typeof window !== "undefined") {
-      window.OrdersCalendarInstance = this;
-    }
+    // 6. API методы
+    this.fetchDeals = function (year, month) {
+      if (!self.state.accessToken) return Promise.resolve({});
 
-    return this;
-  };
+      const startDate = new Date(year, month, 1).toISOString().split("T")[0];
+      const endDate = new Date(year, month + 1, 0).toISOString().split("T")[0];
+
+      return $.ajax({
+        url: `https://${self.state.system.account}.amocrm.ru/api/v4/leads`,
+        data: {
+          "filter[custom_fields_values][field_id]":
+            self.config.FIELD_IDS.ORDER_DATE,
+          "filter[custom_fields_values][from]": startDate,
+          "filter[custom_fields_values][to]": endDate,
+        },
+        headers: {
+          Authorization: `Bearer ${self.state.accessToken}`,
+          "X-Requested-With": "XMLHttpRequest",
+        },
+      })
+        .then(self.processDealsData.bind(self))
+        .catch(function (error) {
+          console.error("Ошибка загрузки сделок:", error);
+          return {};
+        });
+    };
+
+    // 7. Возвращаем публичный интерфейс
+    return {
+      callbacks: this.callbacks,
+      // Другие публичные методы при необходимости
+    };
+  }
 
   return OrdersCalendar;
 });

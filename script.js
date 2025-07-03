c0e423d3a8a7593c1a99f15ab205eaad1c04d67e
Alerom2006 <1a1r120061@gmail.com>
2025-07-03 16:04:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ define(["jquery"], function ($) {
     this.config = {
       widgetInstanceId:
         "orders-calendar-" + Math.random().toString(36).substr(2, 9),
-      version: "1.0.5",
+      version: "1.0.6",
       debugMode: false,
     };
 
